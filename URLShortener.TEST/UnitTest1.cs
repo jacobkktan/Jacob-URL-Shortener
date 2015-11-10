615857f834c586750abb8fd18c5908d6986e6b9a
jacobkktan <jacobkktan@gmail.com>
2015-11-11 06:45:42 +0800

    TEST

---
+++ URLShortener.TEST/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using URLShortener.BL;
+
+namespace URLShortener.TEST
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GenerateCode()
+        {
+            var val = ShortGeneratorHelper.GenerateCode(5);
+            if (string.IsNullOrEmpty(val)) { }
+        }
+
+        [TestMethod]
+        public void CallHelper()
+        {
+            var Helper = new URLShortener.BL.Helper(@"D:\Projects\URLShorternerREST\DB\shortener.db");
+            Helper.InsertRecord("https://tradingpost.my");
+        }
+
+        [TestMethod]
+        public void GetUrl()
+        {
+            var Helper = new URLShortener.BL.Helper(@"D:\Projects\URLShorternerREST\DB\shortener.db");
+            var url = Helper.GetUrl("MMMM");
+        }
+
+        [TestMethod]
+        public void DeleteUrl()
+        {
+            var Helper = new URLShortener.BL.Helper(@"D:\Projects\URLShorternerREST\DB\shortener.db");
+            Helper.DeleteUrl("MMMM");
+        }
+    }
+}
